6c84655d513a72d47c22f37c01f6880a2c808016
SSshadow99 <45866647+SSshadow99@users.noreply.github.com>
2019-01-08 10:28:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,40 +32,9 @@ const Canvas = require('canvas');
 const moment = require('moment');
        
 
- const dateFormat = require('dateformat');
-
-var jimp = require('jimp')
-const request = require('request');
-const tpoints = {};
-const vpoints = {};
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
 
 
-///---------------------------------------------------
+
 
 const welcome = JSON.parse(fs.readFileSync('./welcomer.json' , 'utf8'));
  
