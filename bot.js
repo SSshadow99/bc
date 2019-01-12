b0f750b9f5e5f434d7ebd97d5e81cd8c406cbd63
SSshadow99 <45866647+SSshadow99@users.noreply.github.com>
2019-01-12 12:38:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const gif = require("gif-search");
 const client = new Discord.Client({disableEveryone: true});
 
 
-const prefix = "1";
+const prefix = ".";
 /////////////////////////
 ////////////////////////
 
